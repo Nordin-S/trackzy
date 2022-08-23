1558273578dbca9c537fa2ea366ce0fe8576b848
Nordin-S <nordin.suleimani@gmail.com>
2022-08-23 19:29:23 +0200

    Invitation feature complete, link sent to user by mail upon invitation by admin.
    Nextup > registration using link from invitation email.

--- core/Database.php
+++ core/Database.php
@@ -25,8 +25,8 @@ class Database
         $password = $config['password'] ?? '';
         $this->pdo = new \PDO($host, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(\PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT, false);
         $this->pdo->setAttribute(\PDO::MYSQL_ATTR_SSL_CA, dirname(__DIR__) . 'DigiCertGlobalRootCA.crt.pem');
+        $this->pdo->setAttribute(\PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT, true);
 //        $this->applyMigrations();
     }
 
